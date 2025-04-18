8fddc0c7322bb10a17531b21278129250b69b1e9
Samuel Ogunjimi <42714945+spw3bt3ch@users.noreply.github.com>
2025-04-18 22:37:52 +0000

    Full Data Added
    Mini database added and other things

--- app.py
+++ app.py
@@ -5,10 +5,61 @@ courses = [{}]
 
 app = Flask(__name__, template_folder='templates')
 
+other_courses = [{
+    "id":
+    1,
+    "name":
+    "Python",
+    "description":
+    "Python is a high-level, interpreted programming language.",
+    "price":
+    100,
+    "image":
+    "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png",
+}, {
+    "id":
+    2,
+    "name":
+    "Java",
+    "description":
+    "Java is a high-level, interpreted programming language.",
+    "price":
+    100,
+    "image":
+    "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
+}, {
+    "id":
+    3,
+    "name":
+    "C++",
+    "description":
+    "C++ is a high-level, interpreted programming language.",
+    "price":
+    100,
+    "image":
+    "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
+}, {
+    "id":
+    4,
+    "name":
+    "C#",
+    "description":
+    "C# is a high-level, interpreted programming language.",
+    "price":
+    100,
+    "image":
+    "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
+}, {
+    "id": 5,
+    "name": "Ruby",
+    "description": "Ruby is a high-level, interpreted programming language.",
+    "price": 100,
+}]
+
 
 @app.route('/')
 def index():
-  return render_template('home.html')
+  return render_template('home.html', programming_courses=other_courses)
 
 
 if __name__ == '__main__':
